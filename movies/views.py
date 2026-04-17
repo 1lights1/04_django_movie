7120398e889fded4b1775cee9a66b171ebaa0266
limsehee <sehit6335@gmail.com>
2026-04-17 15:14:11 +0900

    feat: read-delete

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
 
-def index(request): pass
+def index(request):
+    if request.method != 'GET':
+        return redirect('movies:index')
+
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
 def create(request): pass
-def detail(request, pk): pass
+
+def detail(request, pk):
+    if request.method != 'GET':
+        return redirect('movies:index')
+
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movies/detail.html', {'movie': movie})
+
 def update(request, pk): pass
-def delete(request, pk): pass
+
+def delete(request, pk):
+    if request.method == 'POST':
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.delete()
+        return redirect('movies:index')
+    return redirect('movies:detail', pk)
