a52884663ad8e83e8863b2a402d476584be19131
limsehee <sehit6335@gmail.com>
2026-04-17 14:47:23 +0900

    feat: model and form setup

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request): pass
+def create(request): pass
+def detail(request, pk): pass
+def update(request, pk): pass
+def delete(request, pk): pass
